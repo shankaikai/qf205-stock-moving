25c2b7d7a51f3a5b1d40387ba3cb1fb7bf901cc7
Ryan <ryangen97@gmail.com>
2021-10-09 22:53:43 +0800

    updated updateChart() & formatted dates to date objects

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import SMA
 from datetime import datetime
 import csv
 import random
+import datetime as dt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import sys
@@ -9,6 +10,7 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QVBoxLayout
 from PyQt5.QtGui import QIcon
 from PyQt5 import uic
 import matplotlib
+import matplotlib.dates as mdates
 import pandas as pd
 import numpy as np
 matplotlib.use('Qt5Agg')
@@ -55,13 +57,12 @@ class Main(QMainWindow, Ui_MainWindow):
         # Data from CSV parsing
         self.y = pd.DataFrame()
         self.x = pd.DataFrame()
+        self.dateRange = []
 
         self.show()
 
     # When the Update Window button is clicked
     def updateChart(self):
-
-        # TODO: assign close data to self.closeData
 
         # df.rename(columns={'Adj Close': 'Adj_Close'}, inplace=True)
 
@@ -80,50 +81,78 @@ class Main(QMainWindow, Ui_MainWindow):
         crossSell, self.x = SMA.balanceLengths(
             crossSell, self.x)
 
-        print("lengths", len(sma1), len(sma2), len(self.x), len(self.y))
+        # retrieve the current index of the date selected
+        startIdx = self.startDateCombo.currentIndex()
+        endIdx = self.endDateCombo.currentIndex()
 
-        # crossBuy = [0]*(len(self.x) - len(crossBuy)) + crossBuy
-        # crossSell = [0]*(len(self.x) - len(crossSell)) + crossSell
+        # check that the end date is larger than the start date
+        if endIdx < startIdx:
+            print('End date should be later than start date')
+        
+        else:
+            # slice the required data based on date range
+            xDataToPlot = self.dateRange[startIdx:endIdx+1]
+            yDataToPlot = self.y[startIdx:endIdx+1]
+            sma1 = sma1[startIdx:endIdx+1]
+            sma2 = sma2[startIdx:endIdx+1]
+            crossBuy = crossBuy[startIdx:endIdx+1]
+            crossSell = crossSell[startIdx:endIdx+1]
 
-        # sma1 and sma2 are already the same size
-        # crossBuy is an array of 0s and 1s, 1 is the point to buy
-        # crossSell is an array of 0s and -1s, -1 is the point to sell
-        # print(crossBuy, crossSell)
+            # crossBuy = [0]*(len(self.x) - len(crossBuy)) + crossBuy
+            # crossSell = [0]*(len(self.x) - len(crossSell)) + crossSell
 
-        # rolling_mean = df.Adj_Close.rolling(window=15).mean()
-        # rolling_mean2 = df.Adj_Close.rolling(window=50).mean()
+            # sma1 and sma2 are already the same size
+            # crossBuy is an array of 0s and 1s, 1 is the point to buy
+            # crossSell is an array of 0s and -1s, -1 is the point to sell
+            # print(crossBuy, crossSell)
 
-        # TODO: put sma1, sma2, crossBuy, crossSell into graph
+            # rolling_mean = df.Adj_Close.rolling(window=15).mean()
+            # rolling_mean2 = df.Adj_Close.rolling(window=50).mean()
 
-        self.MplWidget.canvas.axes.clear()
-        self.MplWidget.canvas.axes.plot(self.x, self.y)
+            # TODO: put sma1, sma2, crossBuy, crossSell into graph
 
-        if self.sma1CheckBox.isChecked():
-            self.MplWidget.canvas.axes.plot(
-                self.x, sma1, label='15 Day SMA', color='orange')  # placeholder
+            self.MplWidget.canvas.axes.clear()
+            self.MplWidget.canvas.axes.plot(xDataToPlot, yDataToPlot)
 
-        if self.sma2CheckBox.isChecked():
-            self.MplWidget.canvas.axes.plot(
-                self.x, sma2, label='50 Day SMA', color='magenta')  # placeholder
-
-        if self.sma1CheckBox.isChecked() and self.sma2CheckBox.isChecked():
-            self.MplWidget.canvas.axes.plot(self.x, crossBuy)
-            self.MplWidget.canvas.axes.plot(self.x, crossSell)
-            buy_idx = [i for i, e in enumerate(crossBuy) if e == 1]
-            for i in buy_idx:
+            if self.sma1CheckBox.isChecked():
                 self.MplWidget.canvas.axes.plot(
-                    self.x[i], sma1[i], 'go')
+                    xDataToPlot, sma1, label='15 Day SMA', color='orange')  # placeholder
 
-            sell_idx = [i for i, e in enumerate(crossSell) if e == -1]
-            for i in sell_idx:
+            if self.sma2CheckBox.isChecked():
                 self.MplWidget.canvas.axes.plot(
-                    self.x[i], sma1[i], 'ro')
+                    xDataToPlot, sma2, label='50 Day SMA', color='magenta')  # placeholder
 
-        self.MplWidget.canvas.axes.legend(('Close', self.sma1Input.text(
-        ) + 'd', self.sma2Input.text() + 'd'), loc='upper right')
-        print(self.sma2Input.text())
-        self.MplWidget.canvas.axes.set_title('Placeholder')
-        self.MplWidget.canvas.draw()
+            if self.sma1CheckBox.isChecked() and self.sma2CheckBox.isChecked():
+                # self.MplWidget.canvas.axes.plot(xDataToPlot, crossBuy)
+                # self.MplWidget.canvas.axes.plot(xDataToPlot, crossSell)
+                buy_idx = [i for i, e in enumerate(crossBuy) if e == 1]
+                for i in buy_idx:
+                    self.MplWidget.canvas.axes.plot(
+                        xDataToPlot[i], sma1[i], 'go')
+
+                sell_idx = [i for i, e in enumerate(crossSell) if e == -1]
+                for i in sell_idx:
+                    self.MplWidget.canvas.axes.plot(
+                        xDataToPlot[i], sma1[i], 'ro')
+
+            # chart legends
+            self.MplWidget.canvas.axes.legend(('Close', self.sma1Input.text(
+            ) + 'd', self.sma2Input.text() + 'd'), loc='upper right')
+
+            # chart title
+            self.MplWidget.canvas.axes.set_title('Stock Price (' + self.startDateCombo.currentText() + ' - ' + self.endDateCombo.currentText() + ')')
+
+            # axis labels
+            self.MplWidget.canvas.axes.set_xlabel('Dates')
+            self.MplWidget.canvas.axes.set_ylabel('Prices')
+
+            # formate date on x-axis
+            fmt_month = mdates.MonthLocator()
+            
+            self.MplWidget.canvas.axes.xaxis.set_minor_locator(fmt_month)
+            self.MplWidget.canvas.axes.xaxis.set_major_formatter(mdates.DateFormatter('%m-%Y'))
+
+            self.MplWidget.canvas.draw()
 
     # Parse csv and pass data into x data and y data and plot initiate graph
     def loadCsv(self):
@@ -144,6 +173,9 @@ class Main(QMainWindow, Ui_MainWindow):
                 self.startDateCombo.addItems(x)
                 self.endDateCombo.addItems(x)
                 self.endDateCombo.setCurrentIndex(len(x) - 1)
+
+                # convert date string to datetime
+                self.dateRange = [dt.datetime.strptime(date, '%Y-%m-%d').date() for date in x]
                 
                 self.y = y
                 self.x = x
