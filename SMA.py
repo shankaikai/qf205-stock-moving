46f194a471300c5b48dfd9fd578ad4da2f4ddd43
Kaye <tankaye@gmail.com>
2021-11-14 16:25:56 +0800

    change parameter typing for getMovingAverage

--- SMA.py
+++ SMA.py
@@ -10,18 +10,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def getMovingAverage(data, window=int):
+def getMovingAverage(data, window: int):
     weights = np.repeat(1.0, window) / window
-    smas = np.convolve(data, weights, 'valid')
+    smas = np.convolve(data, weights, "valid")
     return smas
 
 
 # Make both SMAs the same length
 def balanceLengths(sma1, sma2):
-    sma1 = np.concatenate((np.array([np.nan]*(len(sma2)-len(sma1))),
-                          sma1)) if len(sma1) <= len(sma2) else sma1
-    sma2 = np.concatenate((np.array([np.nan]*(len(sma1)-len(sma2))),
-                          sma2)) if len(sma2) <= len(sma1) else sma2
+    sma1 = (
+        np.concatenate((np.array([np.nan] * (len(sma2) - len(sma1))), sma1))
+        if len(sma1) <= len(sma2)
+        else sma1
+    )
+    sma2 = (
+        np.concatenate((np.array([np.nan] * (len(sma1) - len(sma2))), sma2))
+        if len(sma2) <= len(sma1)
+        else sma2
+    )
     return sma1, sma2
 
 
@@ -29,7 +35,7 @@ def getCrossovers(sma1, sma2):
     # Compare SMA1 and SMA2 by computing SMA1 - SMA2.If the difference is positive
     # it indicates that SM1 is above SMA2 (1), and vice versa (0)
     # resulting arr is a sequence of 0's and 1's
-    diff = sma1-sma2
+    diff = sma1 - sma2
     # arr = [1 if x >= 0 else 0 for x in diff]
     arr = list(map(lambda x: 1 if x >= 0 else 0, diff))
 
@@ -39,7 +45,7 @@ def getCrossovers(sma1, sma2):
     # in the above mentioned sequence of 0's and 1's.
     # Resulting arr1 is a sequence of values of 0, 1 and -1
 
-    arr1 = [arr[i]-arr[i+1] for i in range(len(arr)-1)]
+    arr1 = [arr[i] - arr[i + 1] for i in range(len(arr) - 1)]
     # Insert a non-crossover point at the first index
     arr1.insert(1, 0)
 
@@ -50,6 +56,7 @@ def getCrossovers(sma1, sma2):
             break
 
     return arr1
+
 
 # Find the locations of 1's and -1's, these are the crossovers
 
